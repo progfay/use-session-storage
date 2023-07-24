3bb92b7d21ab335a293f3fdd666bd0aa4ef125a5
progfay <progfay@gmail.com>
2023-07-25 01:30:24 +0900

    expose `sessionStorage` utility functions

--- lib/index.ts
+++ lib/index.ts
@@ -1,2 +1,7 @@
 export { useSessionStorage } from "./hooks";
+export {
+  getSessionStorageItem,
+  setSessionStorageItem,
+  clearSessionStorageItem,
+} from "./session-storage";
 export type { SessionStorageKey, SessionStorageValue } from "./types";
